d413f6dfa8936f4acc3dce4f034316f6a6049f2d
SDM30 <simondiaz@javeriana.edu.co>
2025-05-25 23:26:54 -0500

    Servidor Respaldo LB responde y envia confirmacion

--- ServidorRespaldo/src/main/java/org/grupo4/redes/ManejadorHealthCheck.java
+++ ServidorRespaldo/src/main/java/org/grupo4/redes/ManejadorHealthCheck.java
@@ -3,7 +3,7 @@ package org.grupo4.redes;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 
-public class ManejadorHealthCheck implements Runnable{
+public class ManejadorHealthCheck {
     private final ZContext context;
     private final String bindAddress;
 
@@ -12,21 +12,20 @@ public class ManejadorHealthCheck implements Runnable{
         this.bindAddress = "tcp://" + ip + ":" + port;
     }
 
-    @Override
-    public void run() {
+    public boolean esperarActivacion() {
         try (ZMQ.Socket socket = context.createSocket(ZMQ.REP)) {
             socket.bind(this.bindAddress);
             System.out.println("[HEALTHCHECK] HealthCheck escuchando en: " + bindAddress);
 
-            while (!Thread.currentThread().isInterrupted()) {
-                byte[] ping = socket.recv(0);
-                if (new String(ping).equals("PING")) {
-                    socket.send("PONG".getBytes(), 0);
-                }
+            byte[] ping = socket.recv(0);
+            if (new String(ping).equals("ACTIVACION")) {
+                socket.send("ACK".getBytes(), 0);
+                return true;
             }
+            return false;
         } catch (Exception e) {
             System.err.println("Error en HealthCheck: " + e.getMessage());
+            return false;
         }
     }
-
 }
