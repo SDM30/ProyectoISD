c53cda1d3ff3c6df849a76c1570f49b7471a6633
SDM30 <simondiaz@javeriana.edu.co>
2025-05-26 19:57:03 -0500

    patron asincrono req-rep

--- Asincrono/src/main/java/org/grupo4/asincrono/servidor/MainServidorAsincrono.java
+++
@@ -1,8 +0,0 @@
-package org.grupo4.asincrono.servidor;
-
-public class MainServidorAsincrono {
-    public static void main(String[] args) {
-        ServidorAsincrono servidor = new ServidorAsincrono();
-        servidor.iniciar();
-    }
-}
